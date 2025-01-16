3dfb90800931eaa0b000f78fffdb7bb427c2af40
BluzeAJ <152267657+AhmadAjiZaelani@users.noreply.github.com>
2025-01-16 22:44:55 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// BOT TELE
-module.exports = {
-  BOT_TOKEN: "8000264460:AAF7jCayehWLDK-CodoCjyoffPnmrUcan6g", // Token bot Telegram
-  OWNER_ID: "6889777508", // ID pemilik bot
-  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
-};
